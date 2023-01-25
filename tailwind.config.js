4e0d44466b417accf51011ae250a96abe899c264
Juniati <114477234+juniati-md@users.noreply.github.com>
2023-01-25 14:16:01 +0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: ["./index.html"],
-  theme: {
-    fontFamily: {
-      'sans': ["inter", "sans-serif"],
-    }
-  },
-  plugins: [],
-}
+    content: ["./index.html"],
+    theme: {
+        fontFamily: {
+            sans: ["Inter", "sans-serif"],
+        },
+        colors: {
+            ...colors,
+            "black-mamba": "#151515",
+            "grey-blind": "#FAFAFA",
+        },
+    },
+    plugins: [],
+};
